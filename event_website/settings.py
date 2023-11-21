abb72979bc1d1ff6c1ca507a5bd325c2025c810a
Johan Hennius <johan.bh19@gmail.com>
2023-11-21 14:43:47 +0000

    Setting up Heroku

--- event_website/settings.py
+++ event_website/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +25,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o#8gt)*0djsueqojxcu&y@w%=rpv3km!g-_xrc+imb@gs@663j'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".gitpod.io"]
+ALLOWED_HOSTS = [".gitpod.io", ".herokuapp.com"]
 
 
 # Application definition
@@ -83,6 +86,11 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.gitpod.io",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- events/models.py
+++ events/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Venue(models.Model):
+
+    name = models.CharField('Venue Name', max_length=120, unique=True)
+    address = models.CharField('Venue Address', max_length=360)
+    post_code = models.CharField('Venue Post Code', max_length=20)
+    email_address = models.EmailField('Venue Email')
+    web_address = models.URLField('Venue Website')
+
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+
+    first_name = models.CharField('User First Name', max_length=60, unique=True)
+    last_name = models.CharField('User Last Name', max_length=60, unique=True)
+    email = models.EmailField('User Email')
+    address = models.CharField('User Address', max_length=360)
+    post_code = models.CharField('User Post Code', max_length=20)
+
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Event(models.Model):
+
+    name = models.CharField('Event Name', max_length=120, unique=True)
+    slug = models.SlugField(max_length=120, unique=True)
+    event_date = models.DateTimeField('Event Date')
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name="event", blank=True)
+    manager =  models.CharField('Event Name', max_length=120)
+    description = models.TextField()
+    attendees = models.ManyToManyField(User, related_name="event_attendee", blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+
