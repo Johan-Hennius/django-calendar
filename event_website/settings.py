5ba2fd6b143b99191e530e6a8d81a96324c37bf0
Johan Hennius <johan.bh19@gmail.com>
2023-11-21 15:22:28 +0000

    Trying to fix heroku deployment

--- event_website/settings.py
+++ event_website/settings.py
@@ -25,12 +25,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".gitpod.io", ".herokuapp.com"]
+ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
