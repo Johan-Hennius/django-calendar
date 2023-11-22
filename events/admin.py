c62d6a6a80054a18cddd5c821674ffbaa3efd828
Johan Hennius <johan.bh19@gmail.com>
2023-11-22 10:25:56 +0000

    Adjust admin area models

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Venue, User, Event
+from .models import Venue, EventUser, Event
 
 # Register your models here.
-admin.site.register(Venue)
-admin.site.register(User)
-admin.site.register(Event)
+# admin.site.register(Venue)
+admin.site.register(EventUser)
+# admin.site.register(Event)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'post_code')
+    order = ('name', )
+    search_fields = ('name', 'address')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'slug', 'venue'), 'description', 'event_date', 'manager')
+    list_display = ('name', 'venue', 'event_date')
+    list_filter = ('event_date', 'venue')
+    prepopulated_fields = {'slug': ('name',)}
--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Venue(models.Model):
@@ -6,21 +7,21 @@ class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120, unique=True)
     address = models.CharField('Venue Address', max_length=360)
     post_code = models.CharField('Venue Post Code', max_length=20)
-    email_address = models.EmailField('Venue Email')
-    web_address = models.URLField('Venue Website')
+    email_address = models.EmailField('Venue Email', blank=True)
+    web_address = models.URLField('Venue Website', blank=True)
 
 
     def __str__(self):
         return self.name
 
 
-class User(models.Model):
+class EventUser(models.Model):
 
     first_name = models.CharField('User First Name', max_length=60, unique=True)
     last_name = models.CharField('User Last Name', max_length=60, unique=True)
     email = models.EmailField('User Email')
-    address = models.CharField('User Address', max_length=360)
-    post_code = models.CharField('User Post Code', max_length=20)
+    address = models.CharField('User Address', max_length=360, blank=True)
+    post_code = models.CharField('User Post Code', max_length=20, blank=True)
 
 
     def __str__(self):
@@ -33,9 +34,9 @@ class Event(models.Model):
     slug = models.SlugField(max_length=120, unique=True)
     event_date = models.DateTimeField('Event Date')
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name="event_location", blank=True)
-    manager =  models.CharField('Manager', max_length=120)
+    manager =  models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField()
-    attendees = models.ManyToManyField(User, related_name="event_attendee", blank=True)
+    attendees = models.ManyToManyField(EventUser, related_name="event_attendee", blank=True)
 
 
     def __str__(self):
